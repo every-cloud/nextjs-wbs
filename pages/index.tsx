76f420110d4cab88db947c509776e9300233bc42
Sejun Kim <zootblue00@gmail.com>
2022-12-10 22:28:49 +0000

    Update 5 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Layout from './components/layout'
+import Head from "next/head";
+import Layout from "./components/layout";
 
 export default function Home() {
   return (
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <h1> 홈입니다</h1>
-
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </Layout>
-  )
+  );
 }
