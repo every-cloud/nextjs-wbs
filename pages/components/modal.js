bac2f9704e9cff738e42a6e88eafed9f3ebc3903
berich <zootblue00@gmail.com>
2023-01-02 17:34:55 +0900

    modal

--- pages/components/modal.js
+++ pages/components/modal.js
@@ -1,18 +1,22 @@
 import React from 'react'
 
-const Modal = ({isVisible, onClose}) => {
+const Modal = ({isVisible, onClose, children}) => {
     if(!isVisible) return null;
 
 
+    const handleClose =(e) => {
+        if(e.target.id ==='wrapper') onClose();
+    }
+
     return (
-        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm'
-            >
-            <div className='w-[600px] flex flex-col'>
+        <div id="wrapper" className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-25 backdrop-blur-sm'
+            onClick={(handleClose)}>
+            <div className='md:w-[600px] w-[90%] mx-auto flex flex-col'>
                 <button onClick={() => onClose()} className='text-xl text-white place-self-end'
                
                 >X</button>
                 <div className='p-2 bg-white rounded'>
-                Modal
+                {children}
                 </div>
             </div>
         </div>
