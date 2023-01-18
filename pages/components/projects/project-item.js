0cd1996f10c6659eaa3fb6496963dfaa7dcf2813
berich <zootblue00@gmail.com>
2023-01-18 13:39:03 +0900

    배포

--- pages/components/projects/project-item.js
+++ pages/components/projects/project-item.js
@@ -1,24 +1,41 @@
 import Image from 'next/legacy/image'
+import { transferableAbortSignal } from 'util'
 
 export default function ProjectItem({data}) {
 
     const title = data.properties.Name.title[0].plain_text
     const description = data.properties.Description.rich_text[0].plain_text
     const imgSrc = data.cover.file?.url || data.cover.external.url 
+    const tags = data.properties.Tags.multi_select
 
 
     return (
-        <div className="flex flex-col p-6 m-3 bg-slate-700 rounded-md">
+        <div className="project-card">
 
             <Image
+                className='rounded-t-xl' 
                 src={data.cover.external.url}
                 width="100%"
                 height="60%"
-            
+                layout="responsive"
+                objectFit='none'
+                quality={100}
+                alt="cover image"
             />
 
-            <h1>{title}</h1>
-            <h3>{description}</h3>
+            <div className='p-4 flex flex-col'>
+                <h1 className='text-2xl font-bold'>{title}</h1>
+                <h3 className='mt-4 text-xl'>{description}</h3>   
+
+                <div className='flex items-start mt-2'>
+                    {tags.map((aTag)=>(
+                        <h1 className='px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30' key={aTag.id}>{aTag.name}</h1>
+                    ))}
+
+                </div>
+
+            </div>
+
         </div>
     )
 }  
--- pages/project.tsx
+++ pages/project.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import Layout from './components/layout'
 import { TOKEN, DATABASE_ID } from '../config'
 import ProjectItem from './components/projects/project-item'
@@ -5,16 +6,29 @@ import ProjectItem from './components/projects/project-item'
 export default function Project({projects}) {
 
 
-
     return (
         <Layout>
+          <div className='
+             flex flex-col items-center justify-center min-h-screen px-6 mb-10'>
 
-       <h1> 총 프로젝트 : { projects.results.length }</h1>
+       
+            <Head>
+                <title>제목표시줄 입니다</title>
+                <meta name="description" content="설명입니다" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-        {projects.results.map((aProject) => (
-         <ProjectItem key={aProject.id} data={aProject}/>
-        ))}
+          <h1 className='text-4xl font-bold sm:text-6xl'> 
+            총 프로젝트 : 
+            <span className='pl-4 text-blue-500'> { projects.results.length }</span>
+            </h1>
 
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8 m-6 py-10">
+              {projects.results.map((aProject) => (
+              <ProjectItem key={aProject.id} data={aProject}/>
+              ))}
+          </div>
+        </div>
         </Layout>
     )
 }
@@ -22,7 +36,7 @@ export default function Project({projects}) {
 /*  빌드 타임에 호출  */
 export async function getStaticProps() {
 
-    const options = {
+    const options = { 
         method: 'POST',
         headers: {
           Accept: 'application/json',
